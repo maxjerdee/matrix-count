a5e3962fc997df1e93868d7d6a5858452e39d2f9
Xiaoran Chen <xiaoranc@umich.edu>
2025-01-16 15:18:25 -0500

    update code

--- run_examples.py
+++ run_examples.py
@@ -2,6 +2,10 @@
 
 import numpy as np
 
-from estimates.linear_time import log_Omega_S_DM
+#from estimates.linear_time import log_Omega_S_DM
 
-print(log_Omega_S_DM(0))
+#print(log_Omega_S_DM(0))
+
+print(np.random.dirichlet(np.ones(5)))
+print(np.random.dirichlet(10 * np.ones(5)))
+print(np.random.dirichlet(10 ** (-3) * np.ones(5)))
